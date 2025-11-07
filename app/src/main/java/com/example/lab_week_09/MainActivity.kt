54ea3238d6d00da6b93a2fb0ea7afccd39a6657a
callistajb <jasmine.kelly2006@gmail.com>
2025-11-07 14:30:39 +0700

    Commit No. 2 – State and Event Handler

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -16,6 +16,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -34,8 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val list = listOf("Tanu", "Tina", "Tono")
-                    Home(list)
+                    Home()
                 }
             }
         }
@@ -43,53 +46,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Home(
-    items: List<String>
+fun Home() {
+    val listData = remember {
+        mutableStateListOf(
+            Student("Tanu"),
+            Student("Tina"),
+            Student("Tono")
+        )
+    }
+    var inputField = remember { mutableStateOf(Student("")) }
+
+    HomeContent(
+        listData,
+        inputField.value,
+        { input -> inputField.value = inputField.value.copy(input) },
+        {
+            if (inputField.value.name.isNotBlank()) {
+                listData.add(inputField.value)
+                inputField.value = Student("")
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeContent(
+    listData: SnapshotStateList<Student>,
+    inputField: Student,
+    onInputValueChange: (String) -> Unit,
+    onButtonClick: () -> Unit
 ) {
     LazyColumn {
-        item{
+        item {
             Column(
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = stringResource(
-                        id = R.string.enter_item)
-                    )
-                    TextField(
-                        value = "",
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number
-                        ),
-                        onValueChange = {
-
-                        }
-                    )
-                    Button(onClick = { }) {
-                        Text(
-                            text = stringResource(
-                                id = R.string.button_click
-                            )
-                        )
+            ) {
+                Text(text = stringResource(id = R.string.enter_item))
+                TextField(
+                    value = inputField.name,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text
+                    ),
+                    onValueChange = {
+                        onInputValueChange(it)
                     }
-                }
-            }
-            items(items) { item ->
-                Column(
-                    modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = item)
+                )
+                Button(onClick = {
+                    onButtonClick()
+                }) {
+                    Text(text = stringResource(id = R.string.button_click))
                 }
             }
         }
+        items(listData) { item ->
+            Column(
+                modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = item.name)
+            }
+        }
     }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome( ) {
     LAB_WEEK_09Theme {
-        Home(listOf("Tanu", "Tina", "Tono"))
+        Home()
     }
 }
+
+data class Student(
+    var name: String
+)
